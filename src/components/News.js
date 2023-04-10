7740df29616a05973694abeb47a3a47ef19bb34b
Krishna Bhosle <kbhosle2000@gmail.com>
2023-04-11 00:59:20 +0530

    React News Application
    News Application to Fetch news from the Newsapi source

--- src/components/News.js
+++ src/components/News.js
@@ -1,84 +1,118 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 
 export class News extends Component {
+  static defaultProps = {
+    pageSize: 12,
+    country: "us",
+    category: "sports",
+  };
 
-  async componentDidMount() {
-
-    let url = "https://newsapi.org/v2/everything?q=apple&from=2023-02-24&to=2023-02-24&sortBy=popularity&apiKey=d2efbac716ef4cd29f9d62237244d3f7&page=1&pageSize=15"
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    // console.log(parsedData);
-    this.setState(
-      {articles:parsedData.articles}
-      )
-  }
+  static propTypes = {
+    pageSize: PropTypes.number,
+    country: PropTypes.string,
+    category: PropTypes.string,
+  };
 
   constructor() {
     super();
     this.state = {
-      articles:[],
-      loading : false,
-      page :1
-    }
+      articles: [],
+      loading: false,
+      page: 1,
+    };
   }
 
-  goToPrev = async ()=>{
-    console.log("clicked on Prev")
-
-    let url = `https://newsapi.org/v2/everything?q=apple&from=2023-02-24&to=2023-02-24&sortBy=popularity&apiKey=d2efbac716ef4cd29f9d62237244d3f7&page=${this.state.page-1}&pageSize=15`
+  async update() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${
+      this.props.category
+    }&apiKey=d2efbac716ef4cd29f9d62237244d3f7&page=${
+      this.state.page
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
 
     this.setState({
-      page:this.state.page - 1,
-      articles:parsedData.articles
-
-    })
+      page: this.state.page,
+      articles: parsedData.articles,
+      loading: false,
+    });
   }
-  
-  goToNext = async ()=> {
-    console.log("clicked on Next")
 
-    let url = `https://newsapi.org/v2/everything?q=apple&from=2023-02-24&to=2023-02-24&sortBy=popularity&apiKey=d2efbac716ef4cd29f9d62237244d3f7&page=${this.state.page+1}&pageSize=15`
-    let data = await fetch(url);
-    let parsedData = await data.json();
+  async componentDidMount() {
+    // let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d2efbac716ef4cd29f9d62237244d3f7`
 
-    this.setState({
-      page:this.state.page + 1,
-      articles:parsedData.articles
-
-    })
+    this.update();
   }
+
+  goToPrev = async () => {
+    // console.log("clicked on Prev")
+    this.setState({page : this.state.page-1});
+    this.update();
+  };
+
+  goToNext = async () => {
+    // console.log("clicked on Next");
+
+    this.setState({page : this.state.page+1});
+    this.update();
+  };
 
   render() {
     return (
-      <div className='container my-3'>
-        <h1>NewsMonkey -Top Headlines</h1>
+      <div className="container my-3">
+        <h1 className="text-center">News 24/7 -Top Headlines</h1>
 
-  
+        {this.state.loading && <Spinner />}
         <div className="row">
-
-          {this.state.articles.map((element)=>{
-            return <div className="col-md-4" key={element.url}>
-            <NewsItem  title={element.title? element.title: ""} description={element.description ? element.description.slice(0,88) : ""} imageUrl ={element.urlToImage?element.urlToImage : "https://heise.cloudimg.io/bound/1200x1200/q85.png-lossy-85.webp-lossy-85.foil1/_www-heise-de_/imgs/18/3/7/0/6/2/1/1/homepod-mini_tre-e3eee5a1b47b6c2b.jpg"} newsUrl={element.url} />
-            
-          </div>
-          })}
-          
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 88)
+                        : ""
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
         </div>
 
         <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            onClick={this.goToPrev}
+            className="btn btn-dark"
+          >
+            &larr; Previous{" "}
+          </button>
 
-        <button disabled={this.state.page <=1} type="button" onClick={this.goToPrev} className="btn btn-dark">&larr; Previous </button>
-
-        <button type="button" onClick={this.goToNext} className="btn btn-dark">Next &rarr;</button>
+          <button
+            type="button"
+            onClick={this.goToNext}
+            className="btn btn-dark"
+          >
+            Next &rarr;
+          </button>
         </div>
-
       </div>
-      
-    )
+    );
   }
 }
 
-export default News
+export default News;
